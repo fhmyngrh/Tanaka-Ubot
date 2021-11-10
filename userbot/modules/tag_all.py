cc47a372e2caa8f1490f9a554fa2bfaea49986bc
fhmyngrh <85100216+fhmyngrh@users.noreply.github.com>
2021-11-10 01:16:47 -0800

    update tag_all.py

--- userbot/modules/tag_all.py
+++ userbot/modules/tag_all.py
@@ -1,11 +1,17 @@
-"""A Plugin to tagall in the chat for @UniBorg and cmd is `.all`"""
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# ReCode by @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
 
 import asyncio
 import random
 import re
 
 from userbot import CMD_HELP, bot
-from userbot.events import register
+from userbot.events import zelda_cmd
 
 usernexp = re.compile(r"@(\w{3,32})\[(.+?)\]")
 nameexp = re.compile(r"\[([\w\S]+)\]\(tg://user\?id=(\d+)\)\[(.+?)\]")
@@ -14,49 +20,11 @@ emoji = "😀 😃 😄 😁 😆 😅 😂 🤣 😭 😗 😙 😚 😘 🥰 �
 )
 
 
-@register(outgoing=True, pattern=r"^all(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from or FlagContainer.is_active:
-        return
-    try:
-        FlagContainer.is_active = True
+class FlagContainer:
+    is_active = False
 
-        args = event.message.text.split(" ", 1)
-        text = args[1] if len(args) > 1 else None
-        chat = await event.get_input_chat()
-        await event.delete()
 
-        tags = list(
-            map(
-                lambda m: f"[{m.first_name}](tg://user?id={m.id})",
-                await event.client.get_participants(chat),
-            ),
-        )
-        jumlah = []
-        async for participant in event.client.iter_participants(chat):
-            if not FlagContainer.is_active:
-                break
-
-            jumlah.append(participant)
-
-            if len(jumlah) == 5:
-                tags = list(
-                    map(
-                        lambda m: f"[{m.first_name}](tg://user?id={m.id})",
-                        jumlah,
-                    ),
-                )
-                jumlah = []
-
-                if text:
-                    tags.append(text)
-
-                await event.client.send_message(event.chat_id, " ".join(tags))
-                await asyncio.sleep(2)
-    finally:
-        FlagContainer.is_active = False
-        
-@register(outgoing=True, pattern=r"^\.mention(?: |$)(.*)")
+@bot.on(zelda_cmd(outgoing=True, pattern=r"mention(?: |$)(.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -67,8 +35,9 @@ async def _(event):
     async for x in bot.iter_participants(chat, 100500):
         mentions += f"[\u2063](tg://user?id={x.id} {query})"
     await bot.send_message(chat, mentions, reply_to=event.message.reply_to_msg_id)
-    
-@register(outgoing=True, pattern=r"^.emojitag(?: |$)(.*)")
+
+
+@bot.on(zelda_cmd(outgoing=True, pattern=r"emojitag(?: |$)(.*)"))
 async def _(event):
     if event.fwd_from or FlagContainer.is_active:
         return
@@ -109,7 +78,50 @@ async def _(event):
                 await asyncio.sleep(2)
     finally:
         FlagContainer.is_active = False
-        
+
+
+@bot.on(zelda_cmd(outgoing=True, pattern=r"all(?: |$)(.*)"))
+async def _(event):
+    if event.fwd_from or FlagContainer.is_active:
+        return
+    try:
+        FlagContainer.is_active = True
+
+        args = event.message.text.split(" ", 1)
+        text = args[1] if len(args) > 1 else None
+        chat = await event.get_input_chat()
+        await event.delete()
+
+        tags = list(
+            map(
+                lambda m: f"[{m.first_name}](tg://user?id={m.id})",
+                await event.client.get_participants(chat),
+            ),
+        )
+        jumlah = []
+        async for participant in event.client.iter_participants(chat):
+            if not FlagContainer.is_active:
+                break
+
+            jumlah.append(participant)
+
+            if len(jumlah) == 5:
+                tags = list(
+                    map(
+                        lambda m: f"[{m.first_name}](tg://user?id={m.id})",
+                        jumlah,
+                    ),
+                )
+                jumlah = []
+
+                if text:
+                    tags.append(text)
+
+                await event.client.send_message(event.chat_id, " ".join(tags))
+                await asyncio.sleep(2)
+    finally:
+        FlagContainer.is_active = False
+
 
 CMD_HELP.update({
     'tagsall':
