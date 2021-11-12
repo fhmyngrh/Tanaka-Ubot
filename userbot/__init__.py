9025ee87d3e67a49c0029c4784e37ecf6a7171ec
fhmyngrh <85100216+fhmyngrh@users.noreply.github.com>
2021-11-12 06:16:56 -0800

    update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -67,6 +67,8 @@ API_HASH = os.environ.get("API_HASH", "")
 # Userbot Session String
 STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
+SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "").split()}
+
 # Logging channel/group ID configuration.
 BOTLOG_CHATID = int(os.environ.get("BOTLOG_CHATID", ""))
 
